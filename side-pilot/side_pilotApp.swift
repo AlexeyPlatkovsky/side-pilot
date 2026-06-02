f14701bb53f8d47515c8c3507ddc1c59b2c521e1
Alexey Platkovsky <alexey.platkovsky@gmail.com>
2026-06-02 20:03:10 +0300

    Initial Commit

---
+++ side-pilot/side_pilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  side_pilotApp.swift
+//  side-pilot
+//
+//  Created by Aleksei Platkovskii on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct side_pilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
